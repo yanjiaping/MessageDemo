1aefb6b123a33960deecb04e94a025178be894cb
yanjiaping <514634128@qq.com>
2019-07-08 15:18:49 +0800

    pom文件注释图片验证代码

--- src/main/java/com/yjp/controller/KaptchaController.java
+++ src/main/java/com/yjp/controller/KaptchaController.java
@@ -17,42 +17,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.code.kaptcha.Producer;
+//import com.google.code.kaptcha.Producer;
 
 @RestController
 public class KaptchaController {
 
-	@Autowired
-	private Producer kaptchaProducer;
-	@Autowired
-	RedisTemplate<String, String> redisTemplate;
-
-	@GetMapping("/kaptchaTest")
-	public void kaptchaTest(String key, HttpServletResponse response) throws Exception {
-		response.setDateHeader("Expires", 0);
-		response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-		response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-		response.setHeader("Pragma", "no-cache");
-		response.setContentType("image/jpeg");
-		// 生成验证码
-		String capText = kaptchaProducer.createText();
-		// 放入redis缓存
-		redisTemplate.opsForValue().set("CODE:" + key, capText.toLowerCase(), 60, TimeUnit.SECONDS);
-		// 向客户端写出
-		BufferedImage bi = kaptchaProducer.createImage(capText);
-		ServletOutputStream out = response.getOutputStream();
-		ImageIO.write(bi, "jpeg", out);
-		out.flush();
-		out.close();
-	}
-
-	@GetMapping("/example")
-	public String getHello(@RequestHeader HttpHeaders headers) {
-		Set<Entry<String, List<String>>> entrySet = headers.entrySet();
-		for (Entry<String, List<String>> entry : entrySet) {
-			System.out.println(entry.getKey() + ":" + entry.getValue());
-		}
-		return "example";
-	}
+	/*
+	 * @Autowired private Producer kaptchaProducer;
+	 * 
+	 * @Autowired RedisTemplate<String, String> redisTemplate;
+	 * 
+	 * @GetMapping("/kaptchaTest") public void kaptchaTest(String key,
+	 * HttpServletResponse response) throws Exception {
+	 * response.setDateHeader("Expires", 0); response.setHeader("Cache-Control",
+	 * "no-store, no-cache, must-revalidate"); response.addHeader("Cache-Control",
+	 * "post-check=0, pre-check=0"); response.setHeader("Pragma", "no-cache");
+	 * response.setContentType("image/jpeg"); // 生成验证码 String capText =
+	 * kaptchaProducer.createText(); // 放入redis缓存
+	 * redisTemplate.opsForValue().set("CODE:" + key, capText.toLowerCase(), 60,
+	 * TimeUnit.SECONDS); // 向客户端写出 BufferedImage bi =
+	 * kaptchaProducer.createImage(capText); ServletOutputStream out =
+	 * response.getOutputStream(); ImageIO.write(bi, "jpeg", out); out.flush();
+	 * out.close(); }
+	 * 
+	 * @GetMapping("/example") public String getHello(@RequestHeader HttpHeaders
+	 * headers) { Set<Entry<String, List<String>>> entrySet = headers.entrySet();
+	 * for (Entry<String, List<String>> entry : entrySet) {
+	 * System.out.println(entry.getKey() + ":" + entry.getValue()); } return
+	 * "example"; }
+	 */
 
 }
