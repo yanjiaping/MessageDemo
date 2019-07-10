1564fedfae716d8a1c0d14da12c4370dc2ceec2b
yanjiaping <514634128@qq.com>
2019-07-10 13:37:14 +0800

    修改注释

--- src/main/java/com/yjp/util/BuildPushPayloadUtil.java
+++ src/main/java/com/yjp/util/BuildPushPayloadUtil.java
@@ -13,22 +13,19 @@ import cn.jpush.api.push.model.notification.Notification;
 
 public class BuildPushPayloadUtil {
 
-	private static final String NOTIFICATION = "NOTIFICATION";
-	private static final String TITLE = "TITLE";
-	private static final String MSG_CONTENT = "MSG_CONTENT";
 	private static final String REGISTRATION_ID = "1104a89792ded9938f7";
 
 	/**
-	 * 快捷地构建推送对象：所有平台，所有设备，内容为 NOTIFICATION 的通知
+	 * 1.快捷地构建推送对象：所有平台，所有设备，内容为 NOTIFICATION 的通知
 	 * 
 	 * @return
 	 */
 	public static PushPayload buildPushObject_all_all_alert() {
-		return PushPayload.alertAll(NOTIFICATION);
+		return PushPayload.alertAll("NOTIFICATION1");
 	}
 
 	/**
-	 * 构建推送对象：所有平台，推送目标是别名为 "alias1"，通知内容为 NOTIFICATION
+	 * 2.构建推送对象：所有平台，推送目标是别名为 "alias1"，通知内容为 NOTIFICATION
 	 * 
 	 * @return
 	 */
@@ -36,12 +33,12 @@ public class BuildPushPayloadUtil {
 		return PushPayload.newBuilder()
 				.setPlatform(Platform.all())
 				.setAudience(Audience.alias("alias1"))
-				.setNotification(Notification.alert(NOTIFICATION))
+				.setNotification(Notification.alert("NOTIFICATION2"))
 				.build();
 	}
 
 	/**
-	 * 构建推送对象：平台是 Android，目标是 tag 为 "tag1" 的设备，内容是 Android 通知 NOTIFICATION，并且标题为 TITLE
+	 * 3.构建推送对象：平台是 Android，目标是 tag 为 "tag1" 的设备，内容是 Android 通知 NOTIFICATION，并且标题为 TITLE
 	 * 
 	 * @return
 	 */
@@ -49,12 +46,12 @@ public class BuildPushPayloadUtil {
 		return PushPayload.newBuilder()
 				.setPlatform(Platform.android())
 				.setAudience(Audience.tag("tag1"))
-				.setNotification(Notification.android(NOTIFICATION, TITLE, null))
+				.setNotification(Notification.android("NOTIFICATION3", "TITLE3", null))
 				.build();
 	}
 
 	/**
-	 * 构建推送对象：平台是 iOS，推送目标是 "tag1", "tag_all" 的交集，推送内容同时包括通知与消息 - 通知信息是 NOTIFICATION，角标数字为
+	 * 4.构建推送对象：平台是 iOS，推送目标是 "tag1", "tag_all" 的交集，推送内容同时包括通知与消息 - 通知信息是 NOTIFICATION，角标数字为
 	 * 5，通知声音为 "happy"，并且附加字段 from = "JPush"；消息内容是 MSG_CONTENT。通知是 APNs 推送通道的，消息是
 	 * JPush 应用内消息通道的。APNs 的推送环境是“生产”（如果不显式设置的话，Library 会默认指定为开发）
 	 * 
@@ -66,13 +63,13 @@ public class BuildPushPayloadUtil {
 				.setAudience(Audience.tag_and("tag1", "tag_all"))
 				.setNotification(Notification.newBuilder()
 						.addPlatformNotification(IosNotification.newBuilder()
-								.setAlert(NOTIFICATION)
+								.setAlert("NOTIFICATION4")
 								.setBadge(5)
 								.setSound("happy")
 								.addExtra("from", "JPush")
 								.build())
 						.build())
-				.setMessage(Message.content(MSG_CONTENT))
+				.setMessage(Message.content("MSG_CONTENT4"))
 				.setOptions(Options.newBuilder()
 						.setApnsProduction(true)
 						.build())
@@ -80,7 +77,7 @@ public class BuildPushPayloadUtil {
 	}
 
 	/**
-	 * 构建推送对象： 平台是 Andorid 与 iOS， 推送目标是 （"tag1" 与 "tag2" 的并集）交（"alias1" 与 "alias2"
+	 * 5.构建推送对象： 平台是 Andorid 与 iOS， 推送目标是 （"tag1" 与 "tag2" 的并集）交（"alias1" 与 "alias2"
 	 * 的并集）， 推送内容是 - 内容为 MSG_CONTENT 的消息，并且附加字段 from = JPush。
 	 * 
 	 * @return
@@ -92,9 +89,9 @@ public class BuildPushPayloadUtil {
 						.addAudienceTarget(AudienceTarget.tag("tag1", "tag2"))
 						.addAudienceTarget(AudienceTarget.alias("alias1"))
 						.build())
-				.setNotification(Notification.alert(NOTIFICATION))
+				.setNotification(Notification.alert("NOTIFICATION5"))
 				.setMessage(Message.newBuilder()
-						.setMsgContent(MSG_CONTENT)
+						.setMsgContent("MSG_CONTENT5")
 						.addExtra("from", "JPush")
 						.build())
 				.build();
@@ -113,7 +110,7 @@ public class BuildPushPayloadUtil {
 //				.setNotification(Notification.alert(ALERT))
 				.setNotification(Notification.android("I'm notification content!", "I'm title", new HashMap<String, String>()))
 				.setMessage(Message.newBuilder()
-						.setMsgContent(MSG_CONTENT)
+						.setMsgContent("MSG_CONTENT6")
 						.addExtra("from", "YJP")
 						.build())
 				.build();
